8a741055a29418927db91e743c3169c4bed25a51
RubaHusam <ruba.husam@outlook.com>
2023-01-24 11:05:37 +0300

    Added login router

--- backend/.history/routers/userRouter_20230124094953.js
+++ backend/.history/routers/userRouter_20230124094953.js
@@ -3,7 +3,6 @@ const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// register
 router.post('/', async (req, res) => {
   try {
     const { email, password, passwordVerify } = req.body;
@@ -51,51 +50,6 @@ router.post('/', async (req, res) => {
     // sign the token
 
     const token = jwt.sign({ user: saveUser._id }, process.env.JWT_SECRET);
-
-    // send the token in HTTP-only cookie
-
-    res
-      .cookie('token', token, {
-        httpOnly: true,
-      })
-      .send();
-  } catch (err) {
-    console.log(err);
-    res.status(500).send();
-  }
-});
-
-// log in
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    // validation
-
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ errorMessage: 'Please enter all required fields.' });
-    }
-
-    const existingUser = await User.findOne({ email });
-
-    if (!existingUser) {
-      return res.status(401).json({ errorMessage: 'Wrong email or password' });
-    }
-
-    const passwordCorrect = await bcrypt.compare(
-      password,
-      existingUser.passwordHash
-    );
-
-    if (!passwordCorrect) {
-      return res.status(401).json({ errorMessage: 'Wrong email or password' });
-    }
-
-    // sign the token
-
-    const token = jwt.sign({ user: existingUser._id }, process.env.JWT_SECRET);
 
     // send the token in HTTP-only cookie
 
